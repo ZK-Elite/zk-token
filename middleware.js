3c6923d574b70c88043f7e169ffaa4b5c4c1c599
ZK-Elite <zkdev.elite@gmail.com>
2024-03-23 09:13:53 +0000

    Update

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ const ratelimit = new Ratelimit({
 
 // Define which routes you want to rate limit
 export const config = {
-  matcher: '/',
+  matcher: '/assets/*',
 }
 
 export default async function middleware(request) {
